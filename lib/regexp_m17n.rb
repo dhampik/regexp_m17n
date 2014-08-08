f4fa5a5d7b9d7464d53b345b99ba3155d13ce3cd
Kirill Kalachev <dhampik@gmail.com>
2014-08-09 01:31:06 +0400

    Avoid encoding change of the original string

--- lib/regexp_m17n.rb
+++ lib/regexp_m17n.rb
@@ -1,6 +1,7 @@
 module RegexpM17N
   def self.non_empty?(str)
-    str.force_encoding(Encoding::BINARY) if str.encoding.dummy?
-    str =~ Regexp.new('^.+$'.encode(str.encoding), Regexp::FIXEDENCODING)
+    strDup = str.dup # avoid changing the encoding of the original string
+    strDup.force_encoding(Encoding::BINARY) if strDup.encoding.dummy?
+    strDup =~ Regexp.new('^.+$'.encode(strDup.encoding), Regexp::FIXEDENCODING)
   end
 end
